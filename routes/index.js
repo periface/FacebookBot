0bbc048057de44b339ebd1bbf2d6f0b861e626ba
Alan Torres <aats91@outlook.com>
2017-02-26 12:43:15 -0600

    webhookroute

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.get('/webhook', function (req, res, next) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+    req.query['hub.verify_token'] === "el_peri") {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
 });
 
 module.exports = router;
